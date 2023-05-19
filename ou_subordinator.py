e795fda45b24e49ba44b275da8caaf1af163e566
Kiril Zelenkovski <zelenkastiot@gmail.com>
2023-05-19 15:01:09 +0200

    Format the code (pep8)

--- ou_subordinator.py
+++ ou_subordinator.py
@@ -40,8 +40,11 @@ def subordinator(alpha, N, s_delta, t_delta):
             W = np.random.exponential(1)
             V = np.random.uniform(-np.pi / 2, +np.pi / 2)
 
-            tau_delta_s = s_delta ** (1 / alpha) * (np.sin(alpha * (V + c1)) / (np.cos(V) ** (1 / alpha))) * (
-                    ((np.cos(V - alpha * (V + c1))) / W) ** ((1 - alpha) / alpha))
+            tau_delta_s = (
+                s_delta ** (1 / alpha)
+                * (np.sin(alpha * (V + c1)) / (np.cos(V) ** (1 / alpha)))
+                * (((np.cos(V - alpha * (V + c1))) / W) ** ((1 - alpha) / alpha))
+            )
 
             ###################################################################### t(s)
             t_euler[s + 1] = t_euler[s] + tau_delta_s
@@ -49,7 +52,9 @@ def subordinator(alpha, N, s_delta, t_delta):
             ###################################################################### x(t)
 
             F = -x_euler[s]
-            x_euler[s + 1] = x_euler[s] + s_delta * F + np.random.normal(scale=s_delta ** (1 / 2))
+            x_euler[s + 1] = (
+                x_euler[s] + s_delta * F + np.random.normal(scale=s_delta ** (1 / 2))
+            )
 
             s = s + 1
 
@@ -59,38 +64,36 @@ def subordinator(alpha, N, s_delta, t_delta):
 
     return x_euler, t_euler
 
+
 # Set parameters for process
-al = 1.0     # stability indicy
-N = 1000     # number of steps
-T = 10       # total time
-dt = T / N   # size of change in t, dt
+al = 1.0  # stability indicy
+N = 1000  # number of steps
+T = 10  # total time
+dt = T / N  # size of change in t, dt
 st = 0.0001  # size of change in s, ds; has to be smaller than dt
 
 # Calculate the process
-x_euler, t_euler = subordinator(alpha=al,
-                                N=N,
-                                s_delta=st,
-                                t_delta=dt)
+x_euler, t_euler = subordinator(alpha=al, N=N, s_delta=st, t_delta=dt)
 
 fig, ax1 = plt.subplots(dpi=150)
 
-ax2 = ax1.twinx() # make a duplicate axis on the right for the s(t)
+ax2 = ax1.twinx()  # make a duplicate axis on the right for the s(t)
 
-time = np.linspace(0, T, N-2)
+time = np.linspace(0, T, N - 2)
 
 # x(t)
-ax1.plot(time, x_euler[0:N-2], "navy")
+ax1.plot(time, x_euler[0 : N - 2], "navy")
 ax1.set_ylabel("x(t)")
 ax1.set_xlabel(r"$t$")
 ax1.set_xlim(0, T)
 
 # s(t)
-ax2.plot(time, t_euler[0:N-2], "k--", alpha=0.5)
+ax2.plot(time, t_euler[0 : N - 2], "k--", alpha=0.5)
 ax2.set_ylabel("s(t)")
 ax2.set_ylim(0, T)
 
 
-plt.title(r"$\alpha=$" + f'{al}')
+plt.title(r"$\alpha=$" + f"{al}")
 plt.tight_layout(pad=1.5)
 # Uncomment if you wish to save the plot
 # plt.savefig(f"plots/{alpha}.png")
@@ -116,10 +119,7 @@ for al in alphas:
     st = 0.0001
 
     # Calculation of both differential eq.
-    x_euler, t_euler = subordinator(alpha=al,
-                                    N=N,
-                                    s_delta=st,
-                                    t_delta=dt)
+    x_euler, t_euler = subordinator(alpha=al, N=N, s_delta=st, t_delta=dt)
 
     # Save values
     x_e.append(x_euler)
@@ -130,12 +130,12 @@ for al in alphas:
 #####################################################
 
 fig, ax = plt.subplots(3, 2, figsize=(13, 13), dpi=300)
-plt.rcParams['savefig.facecolor'] = "0.9"
+plt.rcParams["savefig.facecolor"] = "0.9"
 
 # subplots, access to desired axis using formula: i*2 + j
 for i in range(0, 3):
     for j in range(0, 2):
-        ax[i, j].set_title(r"$\alpha=$" + f'{alphas[i * 2 + j]}')
+        ax[i, j].set_title(r"$\alpha=$" + f"{alphas[i * 2 + j]}")
         x_euler = x_e[i * 2 + j]
         t_euler = t_e[i * 2 + j]
 
@@ -143,10 +143,10 @@ for i in range(0, 3):
 
         time = np.linspace(0, T, N - 2)
 
-        ax[i, j].plot(time, x_euler[0:N - 2], "navy")
+        ax[i, j].plot(time, x_euler[0 : N - 2], "navy")
         ax[i, j].set_ylabel("x(t)")
 
-        ax2.plot(time, t_euler[0:N - 2], "k--", alpha=0.5)
+        ax2.plot(time, t_euler[0 : N - 2], "k--", alpha=0.5)
         ax2.set_ylabel("s(t)")
         ax2.set_ylim(0, T)
 
